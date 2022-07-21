d9a358a545f441d072cca86bbccb59b3812380fb
RamizMuharemovic <ramiz.muharemovic@htecgroup.com>
2022-07-21 18:48:47 +0200

    comi

---
+++ entrypoint.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+try:
+    with open("index.html", "r", encoding='utf-8') as f:
+    text= f.read()
+    url_change = "https://ananasGit.github.test/iam-qa-automation/"
+    new_url = "https://ananasGit.com/"
+    new_html = text.replace(url_change, new_url)
+
+    with open("index.html", "w", encoding='utf-8') as wr:
+        wr.write(new_html)
+except:
+    print('error no file')

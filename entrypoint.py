87af511d663d72e12c76088c861ea09da4f20820
RamizMuharemovic <ramiz.muharemovic@htecgroup.com>
2022-07-21 19:09:33 +0200

    new

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-try:
-    with open("index.html", "r", encoding='utf-8') as f:
-    text= f.read()
-    url_change = "https://ananasGit.github.test/iam-qa-automation/"
-    new_url = "https://ananasGit.com/"
-    new_html = text.replace(url_change, new_url)
+# try:
+#     with open("index.html", "r", encoding='utf-8') as f:
+#     text= f.read()
+#     url_change = "https://ananasGit.github.test/iam-qa-automation/"
+#     new_url = "https://ananasGit.com/"
+#     new_html = text.replace(url_change, new_url)
+#
+#     with open("index.html", "w", encoding='utf-8') as wr:
+#         wr.write(new_html)
+# except:
+#     print('error no file')
 
-    with open("index.html", "w", encoding='utf-8') as wr:
-        wr.write(new_html)
-except:
-    print('error no file')
+print('VOLIM')

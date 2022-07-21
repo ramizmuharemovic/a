d90e25fdbcd046aa13548a4b19719114ca464e91
RamizMuharemovic <ramiz.muharemovic@htecgroup.com>
2022-07-21 20:35:39 +0200

    add to

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import os
 try:
     with open("index.html", "r", encoding='utf-8') as f:
         text= f.read()
-    url_change = "https://ananasGit.github.test/iam-qa-automation/"
-    new_url = "https://ananasGit.com/"
+    #url_change = "https://ananasGit.github.test/iam-qa-automation/"
+    url_change = os.getenv('URL')
+    #new_url = "https://ananasGit.com/"
+    new_url = os.getenv('NEW_URl')
 
     new_html = text.replace(url_change, new_url)
 
